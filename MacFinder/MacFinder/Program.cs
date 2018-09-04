a1265558c284ee6fa67a9209056b4c683ce8d4a7
Vadim <v.kovenko@live.ru>
2018-09-04 06:39:43 +0300

    добавлен параметр -update

--- MacFinder/MacFinder/Program.cs
+++ MacFinder/MacFinder/Program.cs
@@ -10,25 +10,39 @@ namespace Manufacturer
 
         static void Main(string[] args)
         {
-            taskToWait = new TaskCompletionSource<object>();
-            AssemblyLoadContext.Default.Unloading += SigTermEventHandler;
-            Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelHandler);
-            //eventSource.Subscribe(eventSink) or something...
-            taskToWait.Task.Wait();
-            AssemblyLoadContext.Default.Unloading -= SigTermEventHandler;
-            Console.CancelKeyPress -= new ConsoleCancelEventHandler(CancelHandler);
-
-            MacFinder.Load();
-            MacFinder.Run();
-            Task.Delay(2000).Wait();
-            return;
-
-            Console.WriteLine("The public IEEE registry list will be uploaded to the json file");
-            if (Downloader.GetUrl("http://standards-oui.ieee.org/oui36/oui36.csv") && Downloader.GetUrl("http://standards-oui.ieee.org/oui28/mam.csv") && Downloader.GetUrl("http://standards-oui.ieee.org/oui/oui.csv"))
+            if (args.Length >= 1)
             {
-                Downloader.SaveToJson();
+                switch (args[0])
+                {
+                    case "-update":
+                        Console.WriteLine("The public IEEE registry list will be uploaded to the json file");
+                        if (Downloader.GetUrl("http://standards-oui.ieee.org/oui36/oui36.csv") && Downloader.GetUrl("http://standards-oui.ieee.org/oui28/mam.csv") && Downloader.GetUrl("http://standards-oui.ieee.org/oui/oui.csv"))
+                        {
+                            Downloader.SaveToJson();
+                        }
+                        Task.Delay(2000).Wait();
+                        break;
+                    case "--help":
+                        Console.WriteLine("option -update for update MAC address to manufacturer json file");
+                        break;
+                    default:
+                        break;
+                }
             }
-            Task.Delay(2000).Wait();
+            else
+            {
+                taskToWait = new TaskCompletionSource<object>();
+                AssemblyLoadContext.Default.Unloading += SigTermEventHandler;
+                Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelHandler);
+                //eventSource.Subscribe(eventSink) or something...
+                taskToWait.Task.Wait();
+                AssemblyLoadContext.Default.Unloading -= SigTermEventHandler;
+                Console.CancelKeyPress -= new ConsoleCancelEventHandler(CancelHandler);
+
+                MacFinder.Load();
+                MacFinder.Run();
+                Task.Delay(2000).Wait();
+            }
         }
 
         private static void SigTermEventHandler(AssemblyLoadContext obj)
